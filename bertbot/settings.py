e904a2f0733c0da3e4e3ac45b58d9770fceb45f4
Bastien Roques <bastien.roques971@gmail.com>
2021-05-27 09:16:34 +0200

    security update

--- bertbot/settings.py
+++ bertbot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'chv)c-@s-ue69x&o1*a@=49@bwso3rlgoaiusbe$)*w*gwk5u_'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
